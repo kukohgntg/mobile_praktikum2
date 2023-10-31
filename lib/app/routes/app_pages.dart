ac25a0f148f391bb2b32ea1690b97a6102967853
kukohgntg <kukohfr@gmail.com>
2023-10-31 15:58:07 +0700

    first commit

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,41 @@
+import 'package:get/get.dart';
+
+import '../modules/article/bindings/article_binding.dart';
+import '../modules/article/views/article_view.dart';
+import '../modules/discover/bindings/discover_binding.dart';
+import '../modules/discover/views/discover_view.dart';
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+import '../modules/profile/bindings/profile_binding.dart';
+import '../modules/profile/views/profile_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.ARTICLE,
+      page: () => const ArticleView(),
+      binding: ArticleBinding(),
+    ),
+    GetPage(
+      name: _Paths.DISCOVER,
+      page: () => const DiscoverView(),
+      binding: DiscoverBinding(),
+    ),
+    GetPage(
+      name: _Paths.PROFILE,
+      page: () => ProfileView(),
+      binding: ProfileBinding(),
+    ),
+  ];
+}
